51d8b6a498c78101a75d1df2473d74c3ccf46e85
Mg Thant <tzinwin517@gmail.com>
2024-07-07 10:26:09 +0630

    FMongoDB update

--- utils/database.js
+++ utils/database.js
@@ -3,14 +3,24 @@ const mongodbClient = mongodb.MongoClient;
 const dotenv = require("dotenv");
 dotenv.config();
 
+let db;
+
 const mongodbConnector = () => {
   mongodbClient
     .connect(process.env.MONGODB_URL)
     .then((result) => {
       console.log("Connected to MongoDb database.");
+      db = result.db();
       console.log(result);
     })
     .catch((err) => console.log(err));
 };
 
-module.exports = mongodbConnector;
+const getDb = () => {
+    if(db) {
+        return db
+    }
+    throw "No Database"
+}
+
+module.exports = {mongodbConnector, getDb};
