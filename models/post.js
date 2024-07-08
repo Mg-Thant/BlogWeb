2399e281486c0640ad926cf96469b39d7105133b
Mg Thant <tzinwin517@gmail.com>
2024-07-08 19:40:02 +0630

    SMongoDB Update

--- models/post.js
+++ models/post.js
@@ -2,17 +2,25 @@ const mongodb = require("mongodb");
 const { getDb } = require("../utils/database");
 
 module.exports = class Post {
-  constructor(title, description, imgUrl) {
+  constructor(title, description, imgUrl, id) {
     this.title = title;
     this.description = description;
     this.imgUrl = imgUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   create() {
     const db = getDb();
-    return db
-      .collection("posts")
-      .insertOne(this)
+    let dbTemp;
+
+    if (this._id) {
+      dbTemp = db
+        .collection("posts").find({_id : 1})
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbTemp = db.collection("posts").insertOne(this);
+    }
+    return dbTemp
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
   }
@@ -20,8 +28,8 @@ module.exports = class Post {
   static getPosts() {
     const db = getDb();
     return db
-      .collection("posts")
-      .find()
+      .collection("posts", {locale: "en", caseLevel: true})
+      .find().sort({title : 1})
       .toArray()
       .then((posts) => {
         console.log(posts);
@@ -32,9 +40,23 @@ module.exports = class Post {
 
   static getPost(postId) {
     const db = getDb();
-    return db.collection("posts").find({_id : new mongodb.ObjectId(postId)}).next().then((post) => {
+    return db
+      .collection("posts")
+      .find({ _id: new mongodb.ObjectId(postId) })
+      .next()
+      .then((post) => {
         console.log(post);
         return post;
-    }).catch(err => console.log(err))
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static deletePost(postId) {
+    const db = getDb();
+    return db
+      .collection("posts")
+      .deleteOne({ _id: new mongodb.ObjectId(postId) })
+      .then((result) => console.log("Post Deleted"))
+      .catch((err) => console.log(err));
   }
 };
